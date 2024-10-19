bf63a8688d41e1aa08c76390706a1a9f4af70a1d
MitaraiOterai <mitaraioterai@126.com>
2024-10-19 15:20:44 +0800

    it was 9/9

---
+++ lab02/abc.py
@@ -0,0 +1,15 @@
+def cake():
+    print('beets')
+    def pie():
+        print('sweets')
+        return 'cake'
+    return pie
+
+def snake(x,y):
+    if cake == more_cake:
+        return chocolate
+    else:
+        return x + y
+
+higher_order_lambda = lambda f: lambda x: f(x)
+g = lambda x: x*x
